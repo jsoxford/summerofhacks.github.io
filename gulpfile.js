1e91ec0759f3176811ea2e01495641acf74af88a
Ben Foxall <ben@whiteoctober.co.uk>
2015-04-21 20:37:36 +0100

    logos and styles

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,18 @@
 var gulp = require('gulp');
 var connect = require('gulp-connect');
 var minifyHTML = require('gulp-minify-html');
+var less = require('gulp-less');
 
 gulp.task('html', function() {
   gulp.src('src/index.html')
-    .pipe(minifyHTML())
+    .pipe(minifyHTML({quotes:true}))
+    .pipe(gulp.dest('build'))
+    .pipe(connect.reload());
+});
+
+gulp.task('less', function() {
+  gulp.src('src/style.less')
+    .pipe(less())
     .pipe(gulp.dest('build'))
     .pipe(connect.reload());
 });
@@ -13,11 +21,20 @@ gulp.task('html', function() {
 gulp.task('icons', function() {
   gulp
     .src(['src/icons/*'])
-    .pipe(gulp.dest('build'));
+    .pipe(gulp.dest('build'))
+    .pipe(connect.reload());
+});
+
+// flatten icons into root build directory
+gulp.task('images', function() {
+  gulp
+    .src(['src/images/*'])
+    .pipe(gulp.dest('build'))
+    .pipe(connect.reload());
 });
 
  
-gulp.task('serve', ['html'], function() {
+gulp.task('serve', ['build'], function() {
   connect.server({
     root: 'build',
     livereload: true
@@ -25,8 +42,13 @@ gulp.task('serve', ['html'], function() {
 });
 
 
+gulp.task('build', ['html', 'less', 'images', 'icons']);
+
+
 gulp.task('watch', function () {
   gulp.watch(['src/index.html'], ['html']);
+  gulp.watch(['src/style.less'], ['less']);
+  gulp.watch(['src/images/*'], ['images']);
 });
 
 gulp.task('default', ['serve', 'watch']);
