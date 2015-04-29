e8bf6ec311cfb355bad7e635815c2dc5e7ece6fb
Ben Foxall <ben@whiteoctober.co.uk>
2015-04-29 19:56:20 +0100

    Update 2015-04-29T18:56:16.881Z

--- script.js
+++ script.js
@@ -17,3 +17,56 @@ WebFont.load({
     families: ['Fredoka One', 'Roboto']
   }
 });
+
+
+// [].forEach.call(document.querySelectorAll('section'), function(section){
+//   console.log(section)
+
+//   var bounds = section.getBoundingClientRect();
+//   section.style.webkitTransform = 'translateY(' + (bounds.top*-1) + 'px)';
+// })
+
+
+// [].forEach.call(document.querySelectorAll('section'), function(section){
+//   section.style.webkitTransform = '';
+// })
+
+// [].forEach.call(document.querySelectorAll('section'), function(section, i){
+//   console.log(section)
+
+//   var bounds = section.getBoundingClientRect();
+//   section.style.webkitTransform = (i == 2) ? 'translateY(' + (bounds.top*-1) + 'px)' : 'translateY(20px)';
+
+//   section.style.opacity  = (i == 2) ? 1: 0;
+
+// })
+
+
+
+// [].forEach.call(document.querySelectorAll('section'), function(section, i){
+//   console.log(section)
+
+//   var bounds = section.getBoundingClientRect();
+//   section.style.webkitTransform = (i == 1) ? 'translateY(' + (bounds.top*-1) + 'px)' : 'translateY(20px)';
+
+//   section.style.opacity  = (i == 1) ? 1: 0;
+
+// })
+
+
+[].forEach.call(document.querySelectorAll('section'), function(section, i){
+  // console.log(section)
+
+  // var bounds = section.getBoundingClientRect();
+  // section.style.webkitTransform = (i == 1) ? 'translateY(' + (bounds.top*-1) + 'px)' : 'translateY(20px)';
+
+  section.style.opacity  = (i == 1) ? 1 : 0.2;
+
+});
+
+bean.on(document.documentElement, 'click', 'section h1', function(){
+  console.log("DONE")
+});
+
+
+
